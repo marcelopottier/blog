691cef1e73f2c590d8b0890c276c0b3873fa1f37
marcelopottier <marcelopottier@gmail.com>
2022-05-19 16:49:43 -0300

    Finalização do projeto.

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
         <div id="posts-container">
             <?php foreach($posts as $post): ?>
             <div class="post-box">
-                <img src="<?= $BASE_URL ?>/img/<?= $post['img']?>" alt="<?= $post['title']?>">
-                <h2 class="posts-title">
-                    <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id']?>"><?= $post['title']?></a>
+                <img src="<?php echo $BASE_URL; ?>/img/<?php echo $post['img']; ?>" alt="<?php echo $post['title']; ?>">
+                <h2 class="post-title">
+                    <a href="<?php echo $BASE_URL; ?>post.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a>
                 </h2>
-                <p class="post-description"><?= $post['description']?></p>
+                <p class="post-description"><?php echo $post['description']; ?></p>
                 <div class="tags-container">
                 <?php foreach($post['tags'] as $tag): ?>
-                        <a href="#"><?= $tag ?></a>
+                        <a href="#"><?php echo $tag ?></a>
                     <?php endforeach;?>
                 </div>
             </div>
