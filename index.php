62f1158fc26cce79705533297d44c621b51efbcb
marcelopottier <marcelopottier@gmail.com>
2022-05-19 09:38:26 -0300

    Iniciando projeto

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
-//echo "<select>";
-// $i é a variavel inicial "$i < 10" é a condição e $i++ é o contador"
-//for ($i= date("Y"); $i > date("Y")-100; $i--){
-//    echo '<option value="'.$i.'">'.$i.'</option>';
-//}
-$meses = array("Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro");
-
-foreach ($meses as $index => $mes){
-    echo 'A posição do mes é: '.$index.' e o mes é: '.$mes.'<br />';
-}
-
-
+    include_once("templates/header.php");
+?>
+    <main>
+        <div id="title-container">
+            <h1>Blog</h1>
+            <p>O seu blog de viado.</p>
+        </div>
+        <div id="posts-container">
+            <?php foreach($posts as $post): ?>
+            <div class="post-box">
+                <img src="<?= $BASE_URL ?>/img/<?= $post['img']?>" alt="<?= $post['title']?>">
+                <h2 class="posts-title">
+                    <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id']?>"><?= $post['title']?></a>
+                </h2>
+                <p class="post-description"><?= $post['description']?></p>
+                <div class="tags-container">
+                <?php foreach($post['tags'] as $tag): ?>
+                        <a href="#"><?= $tag ?></a>
+                    <?php endforeach;?>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
+<?php
+    include_once("templates/footer.php");
 ?>
